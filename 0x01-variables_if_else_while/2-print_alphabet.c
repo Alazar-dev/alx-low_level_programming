5715bc216c9ff0e1d9ea7aa82a05e24ce52c3512
Alazar Tadesse <mealazartadesse@gmail.com>
2022-03-11 16:55:11 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Returns 0 (successful)
+ */
+
+int main(void)
+{
+char alph;
+
+{
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+}
+}
+
