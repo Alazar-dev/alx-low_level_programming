2275ef0e38040f79d76d2732a778acd0713ec4f9
Alazar Tadesse <mealazartadesse@gmail.com>
2022-03-11 16:55:11 +0300

    print alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Returns 0 (successful)
+ */
+
+int main(void)
+{
+char hpla;
+
+for (hpla = 'z'; hpla >= 'a'; hpla--)
+{
+putchar(hpla);
+}
+putchar('\n');
+
+return (0);
+}
+
