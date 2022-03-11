dfa95b781962750d4e79cf52ec2f5bce77ee561d
Alazar Tadesse <mealazartadesse@gmail.com>
2022-03-11 16:55:11 +0300

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return 0 (successful)
+ */
+
+int main(void)
+{
+char az;
+char AZ;
+
+for (az = 'a'; az <= 'z'; az++)
+{
+putchar(az);
+}
+
+for (AZ = 'A'; AZ <= 'Z'; AZ++)
+{
+putchar(AZ);
+}
+putchar('\n');
+}
